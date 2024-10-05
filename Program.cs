221fd60b8e430a0fed527a0c723dcf7c05002e88
Aakash <Aakash@DESKTOP-9RM1P17>
2024-10-05 17:50:33 +0530

    Added Database connectivity with EF Code 8

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConsoleEFLinq.Models;
+
 Console.WriteLine("Hello, World!");
+var context = new AdventureWorksContext();
+var addresses = context.Addresses.Where(x=>x.PostalCode.Contains("4")).OrderByDescending(x=>x.PostalCode).ToList();
+if (addresses.Any())
+{
+    foreach (var address in addresses)
+    {
+        Console.WriteLine(address.PostalCode);
+    }
+}
